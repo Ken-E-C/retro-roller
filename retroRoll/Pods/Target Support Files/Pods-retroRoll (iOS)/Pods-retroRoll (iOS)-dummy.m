f10e3d715c96f117789db3cea56b0c7099c63324
Ken-E-C <kenecabjr@protonmail.com>
2022-01-13 19:25:32 -0500

    Installed Cocoapods

---
+++ retroRoll/Pods/Target Support Files/Pods-retroRoll (iOS)/Pods-retroRoll (iOS)-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_retroRoll__iOS_ : NSObject
+@end
+@implementation PodsDummy_Pods_retroRoll__iOS_
+@end
---
+++ retroRoll/Pods/Target Support Files/Pods-retroRoll (macOS)/Pods-retroRoll (macOS)-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_retroRoll__macOS_ : NSObject
+@end
+@implementation PodsDummy_Pods_retroRoll__macOS_
+@end
